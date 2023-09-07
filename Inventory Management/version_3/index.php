493a83b68d1a2a888f76c063ce502c94723db9cc
Md. Rajin Mashrur Siam <67337679+rajin-siam@users.noreply.github.com>
2023-09-07 15:35:08 +0600

    Add files via upload

---
+++ Inventory Management/version_3/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory Management System</title>
+    <link rel="stylesheet" href="./css/index.css"> 
+</head>
+<body>
+    <div class="background-image">
+        <div class="heading">
+            <h1>Welcome to the<br> Inventory Management System</h1>
+            <h2>Created By ICT Department</h2>
+        </div>
+        <div class="start">
+            <p><a href="./login.php">Get Started</a></p>
+        </div>
+    </div>
+</body>
+</html>
